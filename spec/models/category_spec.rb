46a1cb5bb9529e9a1d4b8e08a110457640719ec3
Serafeim Maroulis <seramak@gmail.com>
2014-05-13 14:14:39 +0100

    Category table ready

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Category do
+
+  describe "sub categories" do
+    before do
+      @vege = DietaryRequirement.create(:name => 'Vegeterian')
+      @french = Cuisine.create(:name => 'French')
+    end
+
+    it "should be able to accessed by the Category model" do
+      # assert_equals 2, Category.count
+      expect(Category.count).to be (2)
+      expect(Cuisine.first).to eq(@french)
+      expect(DietaryRequirement.first).to eq(@vege)
+    end
+  end
+
+end
