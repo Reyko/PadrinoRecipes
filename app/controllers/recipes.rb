ab8566a80a8a190f5b2046763293347472f630fc
Serafeim Maroulis <seramak@gmail.com>
2014-05-14 10:39:14 +0100

    Adding recipes controller test

---
+++ app/controllers/recipes.rb
@@ -0,0 +1,8 @@
+PadrinoRecipies::App.controllers :recipes do
+  
+  get :new do
+    200
+  end
+  
+
+end
---
+++ spec/app/controllers/recipes_controller_spec.rb
@@ -0,0 +1,28 @@
+#RSPEC TEST
+
+require 'spec_helper'
+
+describe "RecipesController" do
+
+  describe "GET to :new" do
+    before do
+      # Setup code...
+      get '/recipes/new'
+    end
+
+    # This is a test...
+    it "give us a status code of 200" do
+      # Expectations goes in here...
+      expect(last_response.status).to eq(200)
+      expect(last_response.ok?).to(be_true)
+      last_response.status.should eq(200)
+      last_response.ok?.should be_true
+    end
+  end
+
+  # describe "POST to :create do"
+
+
+  # end
+
+end
